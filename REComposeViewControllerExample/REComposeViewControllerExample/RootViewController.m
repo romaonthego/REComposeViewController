4d1d82c233e1943d322b5faef6fae9ecd8832ffb
Roman Efimov <romaonthego@gmail.com>
2012-10-29 17:05:42 -0500

    Update REComposeViewControllerExample/REComposeViewControllerExample/RootViewController.m

--- REComposeViewControllerExample/REComposeViewControllerExample/RootViewController.m
+++ REComposeViewControllerExample/REComposeViewControllerExample/RootViewController.m
@@ -79,7 +79,7 @@
     composeViewController.navigationItem.leftBarButtonItem.tintColor = [UIColor colorWithRed:60/255.0 green:165/255.0 blue:194/255.0 alpha:1];
     composeViewController.navigationItem.rightBarButtonItem.tintColor = [UIColor colorWithRed:29/255.0 green:118/255.0 blue:143/255.0 alpha:1];
     
-    // Alternate use with REComposeViewControllerCompletionHandler
+    // Alternative use with REComposeViewControllerCompletionHandler
     composeViewController.completionHandler = ^(REComposeResult result) {
         if (result == REComposeResultCancelled) {
             NSLog(@"Cancelled");
